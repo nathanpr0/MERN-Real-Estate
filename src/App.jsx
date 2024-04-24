1742886c7e7c25db92af4ffd96b57284e421f7ba
Nahtaniel Iskandar <nathanieliskandarpro@gmail.com>
2024-04-25 03:08:47 +0700

    install ReactJS

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex justify-center items-center h-[200vh]">
+      <div className="bg-blue-500 relative h-96 w-96">
+        <div className="bg-green-500 absolute h-2/4 w-2/4 top-1/4 left-1/4"></div>
+      </div>
+    </div>
+  );
+}
